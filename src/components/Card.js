598ccb3f81996b6343037a06351b6a5640acfaaf
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2020-02-18 21:32:32 +0900

    fix error

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { selectCard, matchCard, hideCard } from '../actions/CardsType'
-import useAsyncHide from '../asyncactions/useAsyncHide'
-import './Card.css'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { selectCard, matchCard, hideCard } from "../actions/CardsType";
+import useAsyncHide from "../asyncactions/useAsyncHide";
+import "./Card.css";
 
 export const Card = ({ card }) => {
   const numberClass = card.selected ? "number-shown" : "number-hidden";
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const asyncHide = useAsyncHide();
 
-  const handleClick = (id, value) => {
-    dispatch(selectCard(id))
-    dispatch(matchCard(id, value))
-    asyncHide(hideCard())
-  }
+  const handleClick = card => {
+    if (!card.matched) {
+      dispatch(selectCard(card.id));
+      dispatch(matchCard(card.id, card.value));
+      asyncHide(hideCard());
+    }
+  };
 
   return (
-    <div className="card" onClick={() => handleClick(card.id, card.value)}>
+    <div className="card" onClick={() => handleClick(card)}>
       <div className={numberClass}>{card.value}</div>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
